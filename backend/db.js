07bb8315eec2ba7707ebc824938bbacb7afce8a1
Riona177 <ronarashiti14@gmail.com>
2026-05-21 08:35:21 +0200

    update

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "password",
   database: "supermarket_db",
   port: 3306
 });
