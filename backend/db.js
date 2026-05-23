4ba9e6e9d31a555e03137ad0af2ec9d4330b8cf9
Riona177 <ronarashiti14@gmail.com>
2026-05-23 10:52:49 +0200

    update

--- backend/db.js
+++ backend/db.js
@@ -3,15 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-<<<<<<< HEAD
-<<<<<<< HEAD
   password: "password",
-=======
-  password: "",
->>>>>>> e2867bcef7aebb25a20fe81e1dc955492f950621
-=======
-  password: "mYsql895?",
->>>>>>> 697f02eaf861ab3a6928287dbe265d226986a2b3
   database: "supermarket_db",
   port: 3306
 });
@@ -20,7 +12,7 @@ db.connect((err) => {
   if (err) {
     console.log("Gabim ne lidhje me databazen:", err);
   } else {
-    console.log("U lidh me databazen");
+    console.log("Lidhja me databazen u realizua me sukses!");
   }
 });
 
